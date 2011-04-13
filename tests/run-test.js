55c6b9268219b474b277c2e5d398ed52dca95740
jbleuzen <zenzen77@gmail.com>
2011-04-13 03:05:39 +0200

    Removing no more needed files

--- tests/run-test.js
+++
@@ -1,9 +0,0 @@
-var puts = require('sys').puts,
-	fs = require('fs'),
-	cssmin = require('../cssmin').cssmin;
-
-var css = fs.readFileSync("test.css", encoding='utf8');
-
-var min = cssmin(css);
-
-puts(min);
